1ff1aa2602833012b4f86af505dd6a50353a339e
mode30 <emmanuelmodebelu@gmail.com>
2026-04-27 11:22:05 +0100

    add bankaccount,user query,and error handling

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,21 @@ struct BankAccount{
     account_number:String,
 }
 fn main() {
-    println!("Hello, world!");
+    let person_1=BankAccount::new("benjamin".to_string(), "carson".to_string(), 1_000_000.0, "234-235-542".to_string());
+    println!("{:#?}",person_1);
+    let person_collapsed=person_1.expect("failed to handle person_1");
+
+    person_collapsed.display_information();
+    // let (bank_account,_)=person_1.display_information();
 }
+
+fn user_query(prompt:&str)->Result<String,io::Error>{
+
+}
+
+
+
+
 
 #[allow(dead_code)]
 #[allow(unused_variables)]
@@ -20,11 +33,29 @@ impl BankAccount {
         // let first_name=first_name.split_whitespace().next().unwrap_or("");
         // let first_name=first_name.split_whitespace().next().unwrap_or("");
         if first_name.is_empty() || last_name.is_empty() || balance<=0.0|| account_number.is_empty(){
+            // return Err(String::from("argument empty, all arguments must be handled"))
             return Err(io::Error::new(io::ErrorKind::InvalidInput, "argument empty, all arguments must be handled"))
         }
+        // (Self{
+        //     first_name, last_name, balance, account_number 
+        // },"Ok".to_string())
         Ok(Self{
             first_name, last_name, balance, account_number 
         })
+    // fn new(first_name:String,last_name:String,balance:f64,account_number:String)->(Self,String){ 
+    // // fn new(first_name:String,last_name:String,balance:f64,account_number:String)->Result<Self,io::Error> { 
+    //     // let first_name=first_name.split_whitespace().next().unwrap_or("");
+    //     // let first_name=first_name.split_whitespace().next().unwrap_or("");
+    //     // if first_name.is_empty() || last_name.is_empty() || balance<=0.0|| account_number.is_empty(){
+    //     //     return Err(String::from("argument empty, all arguments must be handled"))
+    //     //     // return Err(io::Error::new(io::ErrorKind::InvalidInput, "argument empty, all arguments must be handled"))
+    //     // }
+    //     (Self{
+    //         first_name, last_name, balance, account_number 
+    //     },"Ok".to_string())
+    //     // Ok(Self{
+    //     //     first_name, last_name, balance, account_number 
+    //     // })
 
 
     }
@@ -60,5 +91,9 @@ impl BankAccount {
         self.balance-=amount;
         println!("current amount:{:?}",self.balance);
         Ok(self.balance)
+    }
+
+    fn display_information(&self){
+        println!("first name:{}\nlast name:{}\nbalance:{}\naccount number:{}\n",self.first_name,self.last_name,self.balance,self.account_number);
     }
 }
